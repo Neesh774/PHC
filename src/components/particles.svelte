9f8ff3a03e1a307bdf7f4cb13a3773dd34fd0719
Neesh774 <Kanishq0106@gmail.com>
2022-09-08 00:45:43 -0400

    sections

---
+++ src/components/particles.svelte
@@ -0,0 +1,299 @@
+<script lang="ts">
+	import Particles from 'svelte-particles';
+	import { loadFull } from 'tsparticles';
+
+	const particlesConfig = {
+		autoPlay: true,
+		background: {
+			color: {
+				value: '#252429'
+			},
+			opacity: 1
+		},
+		fullScreen: {
+			enable: true,
+			zIndex: 0
+		},
+		detectRetina: true,
+		duration: 0,
+		fpsLimit: 120,
+		interactivity: {
+			detectsOn: 'window',
+			events: {
+				onHover: {
+					enable: true,
+					mode: 'grab',
+					parallax: {
+						enable: true,
+						force: 140,
+						smooth: 10
+					}
+				},
+				resize: true
+			},
+			modes: {
+				slow: {
+					factor: 3,
+					radius: 200
+				},
+				attract: {
+					distance: 200,
+					duration: 0.4,
+					easing: 'ease-out-quad',
+					factor: 1,
+					maxSpeed: 50,
+					speed: 1
+				},
+				bounce: {
+					distance: 200
+				},
+				bubble: {
+					distance: 400,
+					duration: 2,
+					mix: false,
+					opacity: 0.8,
+					size: 40,
+					divs: {
+						distance: 200,
+						duration: 0.4,
+						mix: false,
+						selectors: []
+					}
+				},
+				connect: {
+					distance: 80,
+					links: {
+						opacity: 0.5
+					},
+					radius: 60
+				},
+				grab: {
+					distance: 200,
+					links: {
+						blink: false,
+						consent: false,
+						opacity: 1
+					}
+				},
+				remove: {
+					quantity: 2
+				},
+				repulse: {
+					distance: 200,
+					duration: 0.4,
+					factor: 100,
+					speed: 1,
+					maxSpeed: 50,
+					easing: 'ease-out-quad',
+					divs: {
+						distance: 200,
+						duration: 0.4,
+						factor: 100,
+						speed: 1,
+						maxSpeed: 50,
+						easing: 'ease-out-quad',
+						selectors: []
+					}
+				},
+				trail: {
+					delay: 1,
+					pauseOnStop: false,
+					quantity: 1
+				},
+				light: {
+					area: {
+						gradient: {
+							start: {
+								value: '#ffffff'
+							},
+							stop: {
+								value: '#000000'
+							}
+						},
+						radius: 1000
+					},
+					shadow: {
+						color: {
+							value: '#000000'
+						},
+						length: 2000
+					}
+				}
+			}
+		},
+		manualParticles: [],
+		motion: {
+			disable: false,
+			reduce: {
+				factor: 4,
+				value: true
+			}
+		},
+		particles: {
+			color: {
+				value: '#ffffff'
+			},
+			groups: {},
+			move: {
+				angle: {
+					offset: 0,
+					value: 90
+				},
+				center: {
+					x: 50,
+					y: 50,
+					radius: 0
+				},
+				decay: 0,
+				distance: {},
+				direction: 'none',
+				drift: 0,
+				enable: true,
+				outModes: {
+					default: 'out',
+					bottom: 'out',
+					left: 'out',
+					right: 'out',
+					top: 'out'
+				},
+				random: false,
+				size: false,
+				speed: 0.2,
+				straight: false,
+				vibrate: false,
+				warp: false
+			},
+			number: {
+				density: {
+					enable: true,
+					area: 800,
+					factor: 1000
+				},
+				limit: 0,
+				value: 100
+			},
+			opacity: {
+				random: {
+					enable: true,
+					minimumValue: 0.1
+				},
+				value: {
+					min: 0.1,
+					max: 0.5
+				},
+				animation: {
+					count: 0,
+					enable: true,
+					speed: 1,
+					decay: 0,
+					sync: false,
+					destroy: 'none',
+					startValue: 'random',
+					minimumValue: 0.1
+				}
+			},
+			reduceDuplicates: false,
+			shape: {
+				options: {},
+				type: 'circle'
+			},
+			size: {
+				random: {
+					enable: true,
+					minimumValue: 1
+				},
+				value: {
+					min: 0.1,
+					max: 10
+				},
+				animation: {
+					count: 0,
+					enable: true,
+					speed: 20,
+					decay: 0,
+					sync: false,
+					destroy: 'none',
+					startValue: 'random',
+					minimumValue: 0.1
+				}
+			},
+			stroke: {
+				width: 0
+			},
+			zIndex: {
+				random: {
+					enable: false,
+					minimumValue: 0
+				},
+				value: 0,
+				opacityRate: 1,
+				sizeRate: 1,
+				velocityRate: 1
+			},
+			life: {
+				count: 0,
+				delay: {
+					value: 0,
+					sync: false
+				},
+				duration: {
+					random: {
+						enable: false,
+						minimumValue: 0.0001
+					},
+					value: 0,
+					sync: false
+				}
+			},
+			links: {
+				blink: false,
+				color: {
+					value: '#ffffff'
+				},
+				consent: false,
+				distance: 150,
+				enable: true,
+				frequency: 1,
+				opacity: 0.4,
+				shadow: {
+					blur: 5,
+					color: {
+						value: '#000'
+					},
+					enable: false
+				},
+				triangles: {
+					enable: false,
+					frequency: 1
+				},
+				width: 1,
+				warp: false
+			}
+		},
+		pauseOnBlur: true,
+		pauseOnOutsideViewport: true,
+		responsive: [],
+		style: {},
+		themes: [],
+		zLayers: 100
+	} as any;
+
+	// @ts-ignore
+	const init = async (engine) => {
+		// you can use main to customize the tsParticles instance adding presets or custom shapes
+		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+		// starting from v2 you can add only the features you need reducing the bundle size
+		await loadFull(engine);
+	};
+</script>
+
+<Particles
+	id="tsparticles"
+	options={particlesConfig}
+	particlesInit={async (engine) => {
+		// you can use main to customize the tsParticles instance adding presets or custom shapes
+		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+		// starting from v2 you can add only the features you need reducing the bundle size
+		await loadFull(engine);
+	}}
+/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,130 +1,183 @@
 <script lang="ts">
-	import Particles from 'svelte-particles';
-	import { loadFull } from 'tsparticles';
-
-	let particlesUrl = 'http://foo.bar/particles.json';
-
-	let particlesConfig = {
-		fps_limit: 60,
-		particles: {
-			color: { value: '#ffffff' },
-			line_linked: {
-				color: '#ffffff',
-				distance: 150,
-				enable: true,
-				opacity: 0.35,
-				width: 1
-			},
-			move: {
-				attract: { enable: false, rotateX: 600, rotateY: 1200 },
-				bounce: false,
-				direction: 'none',
-				enable: true,
-				out_mode: 'out',
-				random: false,
-				speed: 2,
-				straight: false
-			},
-			number: { density: { enable: true, value_area: 800 }, value: 80 },
-			opacity: {
-				anim: { enable: true, opacity_min: 0.1, speed: 1, sync: false },
-				random: false,
-				value: 0.35
-			},
-			shape: {
-				character: {
-					fill: false,
-					font: 'Verdana',
-					style: '',
-					value: '*',
-					weight: '400'
-				},
-				polygon: { nb_sides: 5 },
-				stroke: { color: '#000000', width: 0 },
-				type: 'circle'
-			},
-			size: {
-				anim: { enable: false, size_min: 0.1, speed: 40, sync: false },
-				random: true,
-				value: 5
-			}
-		},
-		polygon: {
-			draw: { enable: false, lineColor: '#ffffff', lineWidth: 0.5 },
-			move: { radius: 10 },
-			scale: 1,
-			type: 'none',
-			url: ''
-		},
-		retina_detect: true
-	} as any;
-
-	// @ts-ignore
-	let onParticlesLoaded = (event) => {
-		const particlesContainer = event.detail.particles;
-
-		// you can use particlesContainer to call all the Container class
-		// (from the core library) methods like play, pause, refresh, start, stop
-	};
-
-	// @ts-ignore
-	let particlesInit = async (engine) => {
-		// you can use main to customize the tsParticles instance adding presets or custom shapes
-		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-		// starting from v2 you can add only the features you need reducing the bundle size
-		await loadFull(engine);
-	};
+	import Particles from '../components/particles.svelte';
 </script>
 
-<div class="background">
+<div class="hero">
+	<Particles />
+	<a href="https://hackclub.com/"
+		><img
+			style="position: absolute; top: -2px; left: 10px; border: 0; width: 128px; z-index: 999;"
+			class="logo"
+			src="https://assets.hackclub.com/flag-orpheus-top.svg"
+			alt="Hack Club"
+		/></a
+	>
 	<h1 class="title">Pomperaug Hack Club</h1>
-	<button class="join">Join Now</button>
-	<Particles
-		id="tsparticles"
-		options={particlesConfig}
-		on:particlesLoaded={onParticlesLoaded}
-		{particlesInit}
-	/>
+	<button class="join cta">Join Now</button>
+</div>
+<div class="sections">
+	<section>
+		<div class="text">
+			<h1>What's This?</h1>
+			<p>
+				The Pomperaug Hack Club is an upcoming club of coders interested in making the world a
+				better place using code, whether that's using websites, apps, or even games. We're also a
+				chapter of the international non-profit <a href="https://hackcclub.com">Hack Club</a>. We
+				don't
+				<a href="https://en.wikipedia.org/wiki/Hacker_culture">"hack"</a>, but we're gonna make
+				awesome projects like this website!
+			</p>
+		</div>
+		<div class="media">
+			<iframe
+				width="100%"
+				height="100%"
+				src="https://www.youtube.com/embed/-sxRdKtKNa0"
+				title="Welcome to Hack Club"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				allowfullscreen
+			/>
+		</div>
+	</section>
+	<section>
+		<div class="media inclusion">
+			<h1>You're <br /> <span class="highlight">invited.</span></h1>
+		</div>
+		<div class="text">
+			<h1>Can I Join?</h1>
+			<p>
+				<i> This club is only for coders with more than 6 years of experience.</i>
+				<br /> <br />
+				Just kidding. We're open to <b>all</b> students, regardless of experience or grade. You can come
+				around to any meetings and hang around, and we'll makes sure that everyone new to hacking gets
+				a fun introduction.
+			</p>
+		</div>
+	</section>
+	<div class="cta">
+		<h1 />
+	</div>
 </div>
 
 <style>
-	.background {
+	@keyframes logoWave {
+		50% {
+			transform: rotate(-5deg);
+		}
+	}
+	.logo {
+		transform-origin: 0% 0%;
+	}
+	.logo:hover {
+		animation: logoWave 1s linear infinite alternate;
+	}
+	.hero {
 		width: 100%;
-		height: 100%;
-		background: rgb(var(--background));
+		height: 80%;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		flex-direction: column;
 		gap: 2rem;
 	}
-	* {
+
+	.title {
+		z-index: 10;
+		font-size: 3rem;
+		text-align: center;
+		font-family: 'Secular One', sans-serif;
 		color: #e5e5e5;
 	}
 
-	.title {
-		font-size: 3rem;
-		text-align: center;
-	}
-
 	.join {
-		--distance: 4px;
+		z-index: 10;
 		display: inline-block;
 		outline: 0;
 		border: 0;
 		cursor: pointer;
 		color: #fff;
 		font-weight: 900;
-		font-size: 14px;
-		height: 2.4rem;
-		padding: 0px 1.4rem;
+		font-size: 20px;
+		height: 3rem;
+		padding: 0px 2.6rem;
 		text-shadow: rgb(0 0 0 / 25%) 0px 3px 8px;
 		transition: all 0.12s ease 0s;
-		background: linear-gradient(90.99deg, #59a5ff 7.06%, #7847ff 56.44%, #8961ff);
 	}
 
 	.join:hover {
-		box-shadow: rgb(80 63 205 / 80%) 0px 1px 80px, -4px 6px 0 1px rgb(153, 0, 255);
+		box-shadow: -4px 6px 0 1px var(--orange);
 		transform: translate(4px, -6px);
+	}
+
+	.sections {
+		z-index: 10;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.sections section {
+		z-index: 10;
+		width: 100%;
+		min-height: 20rem;
+		display: flex;
+		flex-direction: row;
+		background-color: var(--snow);
+	}
+
+	.sections section div.text {
+		width: 50%;
+		padding: 6rem 6rem;
+	}
+
+	.sections section div.media {
+		min-width: 50%;
+		padding: 2rem 4rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.sections section .inclusion {
+		font-family: 'Cabin';
+		font-weight: bold;
+		font-size: 2rem;
+		color: rgb(228, 45, 66);
+	}
+
+	.highlight {
+		box-sizing: border-box;
+		margin: 0;
+		min-width: 0;
+		background-color: rgb(228, 115, 45);
+		-webkit-clip-path: polygon(4% 0%, 100% 0%, 96% 100%, 0% 100%);
+		clip-path: polygon(4% 0%, 100% 0%, 96% 100%, 0% 100%);
+		color: rgb(255, 255, 255);
+		display: inline-block;
+		padding-bottom: 12px;
+		padding-left: 18px;
+		padding-right: 18px;
+		margin-left: 2rem;
+	}
+
+	@media (max-width: 768px) {
+		.sections section {
+			flex-direction: column;
+		}
+
+		.sections section div.text {
+			width: 100%;
+			padding: 2rem 2rem;
+		}
+
+		.sections section div.media {
+			width: 100%;
+			padding: 0 2rem;
+		}
+
+		iframe {
+			height: 14rem;
+		}
 	}
 </style>
